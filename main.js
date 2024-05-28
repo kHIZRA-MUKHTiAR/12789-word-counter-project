fcb202913090c3c7deef7b63980ed14113d5f41d
kHIZRA-MUKHTiAR <161032197+kHIZRA-MUKHTiAR@users.noreply.github.com>
2024-05-28 01:13:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+// this line is called a shebang, it tells os to run it with node.js
+// import inquirer module , which is a command line interface for Node.js
+import inquirer from "inquirer";
+// declare a constant `answer` and assign it to the result of inquirer. prompt function
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
